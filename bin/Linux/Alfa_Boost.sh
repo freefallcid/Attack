8d04411e3fb3cdabcf4d66ac893c9d7d74464aba
Dominic Reich <freefallcid@users.noreply.github.com>
2017-09-17 02:33:07 +0200

    updates Alfa_Boost.sh; adds /guides folder…   with initial files

--- bin/Linux/Alfa_Boost.sh
+++ bin/Linux/Alfa_Boost.sh
@@ -11,12 +11,14 @@ fi
 ifconfig $WLAN down
 #macchanger -A $WLAN
 iw reg set BO
-ifconfig $WLAN up
+#ifconfig $WLAN up
 iwconfig $WLAN channel 13
 iwconfig $WLAN txpower 30
 iwconfig $WLAN rate 1M
-airmon-ng start $WLAN
-ifconfig ${WLAN}mon down
-macchanger -A ${WLAN}mon
-ifconfig ${WLAN}mon up
+#airmon-ng start $WLAN
+#ifconfig ${WLAN}mon down
+#macchanger -A ${WLAN}mon
+#ifconfig ${WLAN}mon up
 
+macchanger -A ${WLAN}
+ifconfig ${WLAN} up
